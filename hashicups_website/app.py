04537bce8bb816629ccd55013b9d3d639cd44844
chee <yeqi2008@gmail.com>
2025-08-29 14:27:21 +0800

    Update app.py

--- hashicups_website/app.py
+++ hashicups_website/app.py
@@ -228,4 +228,4 @@ def healthcheck():
     return jsonify({"status": "healthy", "timestamp": datetime.now().isoformat()})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
